2cb0a8e51cc7109572b4312aee6313bc5700b477
Daniel Lawrence <danny.p.lawrence@gmail.com>
2015-03-17 18:46:58 +0000

    dd rtime and exit form

---
+++ exitscript.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+$(function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      yearRange: "-80:-18",
+    });
+  });
+
+
+$("#send").click(function(){
+       partData=$("#form1").serializeArray();
+       console.log(partData);
+       console.log(sessvars.myObj);
+
+
+       //$.post("http://blake.ppls.ed.ac.uk/~s1122689/script.php",
+//    {
+ //       inputData: output
+ //   },
+ //   function(data, status){
+  //      alert("Data: " + data + "\nStatus: " + status);
+  //  });
+window.location.replace("debrief.html");
+    });
+
+});
